092c7e005b70528617e561fa3d93a6226722edbe
haiqin.yun <haiqin.yun@nenglian.com>
2024-09-18 17:40:12 +0800

    feat: update

--- packages/ls-components/src/views/form/Index.vue
+++ packages/ls-components/src/views/form/Index.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-// import Form from '../../../components/form/Form.vue';
 const FormRef = ref();
+const FormRef1 = ref();
 const loading = ref(false);
 const formData = ref({
   username: '',
   password: ''
 });
-
 const formItems = ref<FormItemsType[]>([
   {
     type: 'input',
@@ -48,6 +46,52 @@ const formItems = ref<FormItemsType[]>([
     ]
   }
 ]);
+
+const formData1 = ref({
+  username: '',
+  password: ''
+});
+const formItems1 = ref<FormItemsType[]>([
+  {
+    type: 'input',
+    label: '用户名',
+    prop: 'username',
+    rules: [
+      {
+        required: true,
+        message: '请输入用户名',
+        trigger: 'blur'
+      },
+      {
+        pattern: /^[A-Z0-9]{4,36}$/i,
+        message: '4-36位数字字母组合',
+        trigger: 'blur'
+      }
+    ]
+  },
+  {
+    type: 'input',
+    label: '密码',
+    prop: 'password',
+    attrs: {
+      'show-password': true,
+      maxlength: 24
+    },
+    rules: [
+      {
+        required: true,
+        message: '请输入用户密码',
+        trigger: 'blur'
+      },
+      {
+        pattern: /^[A-Z0-9@]*$/i,
+        message: '8-24位数字大小写字母组合',
+        trigger: 'blur'
+      }
+    ]
+  }
+]);
+
 function onLogin() {
   if (loading.value) return;
   FormRef.value.validate().then((res: any) => {
@@ -59,21 +103,6 @@ function onLogin() {
     }, 2000);
   });
 }
-
-// const form = reactive({
-//   name: '',
-//   region: '',
-//   date1: '',
-//   date2: '',
-//   delivery: false,
-//   type: [],
-//   resource: '',
-//   desc: ''
-// });
-
-// const onSubmit = () => {
-//   console.log('submit!');
-// };
 </script>
 
 <template>
@@ -82,13 +111,27 @@ function onLogin() {
     label-position="top"
     :loading="loading"
     :colon="false"
-    :show-buttons="false"
+    :show-buttons="true"
+    :show-reset="false"
+    confirm-text="登录"
     :form-data="formData"
     :form-items="formItems"
     :hide-required-asterisk="true"
   />
-  <el-button type="primary" :loading="loading" :disabled="loading" @click="onLogin">登 录</el-button>
-  <!-- <div>--------------------------------------------------------------------------------------------</div> -->
+
+  <div>--------------------------------------------------------------------------------------------</div>
+  <LSForm
+    ref="FormRef1"
+    label-position="top"
+    :loading="loading"
+    :colon="false"
+    :show-buttons="true"
+    :form-data="formData1"
+    :form-items="formItems1"
+    :hide-required-asterisk="true"
+    @submit="onLogin"
+  />
+  <!-- <el-button type="primary" :loading="loading" :disabled="loading" @click="onLogin">登 录</el-button> -->
 </template>
 
 <style scoped></style>
